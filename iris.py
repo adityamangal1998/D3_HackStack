e6412143c2514f68c101710867d43f102da6a85f
Himanshu khanda <90919732+Himanshu1135@users.noreply.github.com>
2022-06-11 13:04:46 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -46,76 +46,101 @@ with mp_face_mesh.FaceMesh(
             cv.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (0,255,0), 1, cv.LINE_AA)
 
             # draw lines
-            # for i in range(16):
-            #     cv.line(frame, mesh_points[RIGHT_EYE[i-1]],mesh_points[RIGHT_EYE[i]],(0,255,0), 1)
-            # for i in range(16):
-            #     cv.line(frame, mesh_points[LEFT_EYE[i-1]],mesh_points[LEFT_EYE[i]],(0,255,0), 1)
+            for i in range(16):
+                cv.line(frame, mesh_points[RIGHT_EYE[i-1]],mesh_points[RIGHT_EYE[i]],(0,255,0), 1)
+            for i in range(16):
+                cv.line(frame, mesh_points[LEFT_EYE[i-1]],mesh_points[LEFT_EYE[i]],(0,255,0), 1)
 
-            # creating mask
-            mask = np.zeros(frame.shape[0:2], dtype=np.uint8)
-            l_points = np.array([np.array(mesh_points[LEFT_EYE])])
-            r_points = np.array([np.array(mesh_points[RIGHT_EYE])])
-            # method 1 smooth region
-            cv.drawContours(mask, [l_points,r_points], -1, (255, 255, 255), -1, cv.LINE_AA)
-            # method 2 not so smooth region
-            # cv.fillPoly(mask, points, (255))
-            res = cv.bitwise_and(frame, frame, mask=mask)
-            rect = cv.boundingRect(l_points)  # returns (x,y,w,h) of the rect
-            rect = cv.boundingRect(r_points)  # returns (x,y,w,h) of the rect
-            cropped = res[rect[1]: rect[1] + rect[3], rect[0]: rect[0] + rect[2]]
+            # # creating mask
+            # mask = np.ones(frame.shape[0:2], dtype=np.uint8)
+            # l_points = np.array([np.array(mesh_points[LEFT_EYE])])
+            # r_points = np.array([np.array(mesh_points[RIGHT_EYE])])
+            # # method 1 smooth region
+            # cv.drawContours(mask, [l_points,r_points], -1, (255, 255, 255), -1, cv.LINE_AA)
+            # # method 2 not so smooth region
+            # # cv.fillPoly(mask, points, (255))
+            # res = cv.bitwise_and(frame, frame, mask=mask)
+            # rect = cv.boundingRect(l_points)  # returns (x,y,w,h) of the rect
+            # rect = cv.boundingRect(r_points)  # returns (x,y,w,h) of the rect
+            # cropped = res[rect[1]: rect[1] + rect[3], rect[0]: rect[0] + rect[2]]
+            # 
+            # 
+            # ## crate the white background of the same size of original image
+            # wbg = np.ones_like(frame, np.uint8) * 255
+            # cv.bitwise_not(wbg, wbg, mask=mask)
+            # # overlap the resulted cropped image on the white background
+            # dst = wbg + res
+            # cropped = sr.upsample(cropped)
+            # cropped = cv.cvtColor(cropped, cv.COLOR_RGB2GRAY)
+            # cropped = cv.equalizeHist(cropped)
+            # 
+            # image = cropped
+            # 
+            # # image = cv.cvtColor(image,cv.COLOR_RGB2GRAY)
+            # print("Shape of cropped window",image.shape)
+            # # Smoothing
+            # kernel = np.ones((3, 3), np.float32) / 9
+            # image = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
+            # # image = cv.filter2D(image, -1, kernel)
+            # # image = cv.blur(image, (5, 5))
+            # # image = cv.GaussianBlur(image, (5, 5), 0)
+            # # image = cv.medianBlur(image, 3)
+            # image = cv.bilateralFilter(image,3, 75, 75)
+            # 
+            # # THRESHOLDING
+            # # sauvola = threshold_sauvola(image, window_size=25)
+            # # adaptive = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, -2)
+            # _, binary = cv.threshold(image, 100, 255, cv.THRESH_BINARY)
+            # _, otsu = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+            # contours, hierarchy = cv.findContours(otsu, 1, 2)
+            # otsu = cv.resize(otsu,(100,50),interpolation=cv.INTER_NEAREST)
+            # print(len(contours))
+            # for i in contours:
+            #     area = cv.contourArea(i)
+            #     if area > 1500:
+            #         rect = cv.minAreaRect(i)
+            #         box = cv.boxPoints(rect)
+            #         box = np.int0(box)
+            #         # otsu = cv.drawContours(otsu, [box], -1, (0, 255, 0), 2)
+            #         print(area)
+            # 
+            #     else:
+            #         epsilon = 0.1 * cv.arcLength(i, True)
+            #         approx = cv.approxPolyDP(i, epsilon, True)
+            #         otsu = cv.fillPoly(otsu, [approx], color=(255, 255, 255))
+            # # Black pixel count
+            # count = cv.countNonZero(otsu)
+            # print(" zero count", otsu.size - count)
 
-            ## crate the white background of the same size of original image
-            wbg = np.ones_like(frame, np.uint8) * 255
-            cv.bitwise_not(wbg, wbg, mask=mask)
-            # overlap the resulted cropped image on the white background
-            dst = wbg + res
-            cropped = sr.upsample(cropped)
-            cropped = cv.cvtColor(cropped, cv.COLOR_RGB2GRAY)
-            cropped = cv.equalizeHist(cropped)
+ # 2 Approach = color Contour
 
-            image = cropped
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        # define range of blue color in HSV
+        lower_hsv = np.array([50, 100, 100])
+        upper_hsv = np.array([70, 255, 255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv.inRange(hsv, lower_hsv, upper_hsv)
 
-            # image = cv.cvtColor(image,cv.COLOR_RGB2GRAY)
-            print("Shape of cropped window",image.shape)
-            # Smoothing
-            kernel = np.ones((3, 3), np.float32) / 9
-            image = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
-            # image = cv.filter2D(image, -1, kernel)
-            # image = cv.blur(image, (5, 5))
-            # image = cv.GaussianBlur(image, (5, 5), 0)
-            # image = cv.medianBlur(image, 3)
-            image = cv.bilateralFilter(image,3, 75, 75)
-
-            # THRESHOLDING
-            # sauvola = threshold_sauvola(image, window_size=25)
-            # adaptive = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, -2)
-            _, binary = cv.threshold(image, 100, 255, cv.THRESH_BINARY)
-            _, otsu = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-            contours, hierarchy = cv.findContours(otsu, 1, 2)
-            otsu = cv.resize(otsu,(100,50),interpolation=cv.INTER_NEAREST)
-            print(len(contours))
-            for i in contours:
-                area = cv.contourArea(i)
-                if area > 1000:
-                    rect = cv.minAreaRect(i)
-                    box = cv.boxPoints(rect)
-                    box = np.int0(box)
-                    # otsu = cv.drawContours(otsu, [box], -1, (0, 255, 0), 2)
-                    print(area)
-
-                else:
-                    epsilon = 0.1 * cv.arcLength(i, True)
-                    approx = cv.approxPolyDP(i, epsilon, True)
-                    otsu =cv.fillPoly(otsu, [approx], color=(255, 255, 255))
-            # Black pixel count
-            count = cv.countNonZero(otsu)
-            print(" zero count", otsu.size - count)
-
+        # Bitwise-AND mask and original image
+        res = cv.bitwise_and(frame, frame, mask=mask)
+        ret, thresh = cv.threshold(mask, 127, 255, 0)
+        contours, hierarchy = cv.findContours(thresh, 1, 2)
+        print(len(contours))
+        otsu = np.zeros((700, 700), np.uint8)
+        for i in contours:
+            # epsilon = 0.1 * cv.arcLength(i, True)
+            # approx = cv.approxPolyDP(i, epsilon, True)
+            rect = cv.minAreaRect(i)
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            otsu = cv.drawContours(otsu, [box], -1, (255, 0, 0), 1)
+            print(box)
+            cropped = cv.resize(frame,box[2],interpolation=cv.INTER_NEAREST)
 
         cv.imshow('Frame', frame)
         # cv.imshow("mask",mask)
         # cv.imshow('res',res)
-        # cv.imshow('new', cropped)
+        cv.imshow('new', cropped)
         cv.imshow('Threshold',otsu)
 
         key = cv.waitKey(1)
